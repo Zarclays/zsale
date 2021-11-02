ff7064c51d522a3d8d00d9dea6483af7f45824a0
Amire Joseph <operacongahub@gmail.com>
2021-11-02 10:39:55 +0100

    Update ConfirmAddress.sol

--- ContractCode/contracts/ConfirmAddress.sol
+++ ContractCode/contracts/ConfirmAddress.sol
@@ -8,13 +8,6 @@ contract ConfirmAddress{
     
     
     // to dectect if the address is a contract or not
-    function isContract(address account) internal view returns(bool){
-        uint256 size;
-        assembly{
-            size := extcodesize(account)
-        }
-        
-        return size > 0;
    /**
      * @dev Returns true if `account` is a contract.
      *
