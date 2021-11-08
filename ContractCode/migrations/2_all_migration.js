314c8ca54588952212bb78330af7036849068199
Amire Joseph <91990226+Operaconga14@users.noreply.github.com>
2021-11-08 15:36:54 +0100

    Delete 2_all_migration.js

--- ContractCode/migrations/2_all_migration.js
+++
@@ -1,9 +0,0 @@
-var TokenExample = artifacts.reqiure("./TokenExample.sol");
-var CampaignSale = artifacts.require("./CampaignSale.sol");
-
-module.export = function(deplyer){
-    deployer.deploy(TokenExample, 1000000000000000000).then(function(){
-        return deployer.deploy(CampaignSale, TokenExample.address);
-    });
-    
-}
