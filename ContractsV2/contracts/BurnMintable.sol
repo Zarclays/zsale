5896ac97d12819379b0a730ed0372109e169ad44
Joseph <operacongahub@gmail.com>
2022-05-17 18:53:43 +0100

    updates

--- ContractsV2/contracts/BurnMintable.sol
+++ ContractsV2/contracts/BurnMintable.sol
@@ -1,3 +1,18 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+
+contract BurnMintable is ERC20PresetMinterPauser{
+
+    mapping(address => uint256) private _balances;
+
+    constructor(string memory name, string memory symbol, uint256 totalSupply, uint decimals) ERC20PresetMinterPauser(name, symbol){
+        _mint(msg.sender, totalSupply);
+    }
+   
+}
--- ContractsV2/contracts/StandardERC20.sol
+++ ContractsV2/contracts/StandardERC20.sol
@@ -1,3 +1,14 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract StandardERC20 is ERC20{
+
+    // uint public decimals;
+    constructor(string memory name, string memory symbol, uint decimals, uint256 totalSupply) ERC20(name, symbol){
+        _mint(msg.sender, totalSupply);
+        // it should print out authors decimal not default erc20 decimal till woking on it
+    }
+}
