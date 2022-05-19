d3f0a65de513ab47ab0bd850bac275bbd304a3a4
Joseph <operacongahub@gmail.com>
2022-05-19 17:15:10 +0100

    updates

--- ContractsV2/contracts/BurnMintable.sol
+++ ContractsV2/contracts/BurnMintable.sol
@@ -9,14 +9,14 @@ import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol"
 
 contract BurnMintable is ERC20PresetMinterPauser{
 
-    uint8 _decimals
+    uint8 _decimals;
 
-    constructor(string memory name, string memory symbol, uint256 totalSupply, uint8 decimals) ERC20PresetMinterPauser(name, symbol){
+    constructor(string memory name, string memory symbol, uint8 decimals, uint256 totalSupply) ERC20PresetMinterPauser(name, symbol){
         _mint(msg.sender, totalSupply);
         _decimals = decimals;
     }
-   
-   function decimals() public view virtual override returns (uint8) {
+
+    function decimals() public view virtual override returns(uint8){
         return _decimals;
     }
 }
