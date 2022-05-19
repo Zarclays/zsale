991af875115762707542a40516bcf77a452711b2
Joseph <operacongahub@gmail.com>
2022-05-19 17:24:46 +0100

    Update BurnMintable.sol

--- ContractsV2/contracts/BurnMintable.sol
+++ ContractsV2/contracts/BurnMintable.sol
@@ -2,9 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract BurnMintable is ERC20PresetMinterPauser{
